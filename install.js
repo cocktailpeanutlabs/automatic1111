b290991531158242b33f44372c800979940b82f6
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-02 17:05:36 -0500

    windows handling

--- install.js
+++ install.js
@@ -37,8 +37,8 @@ module.exports = async (kernel) => {
       "method": "fs.share",
       "params": {
         "drive": {
-          "torch": "app/venv/lib/python3.10/site-packages/torch",
-          "torchvision": "app/venv/lib/python3.10/site-packages/torchvision",
+          "torch": "{{platform === 'win32' ? 'app/venv/Lib/site-packages/torch' : 'app/venv/lib/python3.10/site-packages/torch'}}",
+          "torchvision": "{{platform === 'win32' ? 'app/venv/Lib/site-packages/torchvision' : 'app/venv/lib/python3.10/site-packages/torchvision'}}",
           "checkpoints": "app/models/Stable-diffusion",
 //          "configs": "app/models/Stable-diffusion",
           "vae": "app/models/VAE",
